71a7bc4b5796679fbeb0a3e1744e02ef355948ea
quaelek <quaelek@Ubuntu.myguest.virtualbox.org>
2024-03-03 18:20:53 -0500

    Committer: quaelek <quaelek@Ubuntu.myguest.virtualbox.org>
     On branch master
     Changes to be committed:
    	modified:   conf/username.txt
    	modified:   examples/autotest-validate/autotest-validate.c
    	modified:   student-test/assignment1/Test_validate_username.c
    
     Changes not staged for commit:
    	modified:   assignment-autotest (modified content)
    
     Untracked files:
    	finder-app/finder.sh
    	finder-app/writer.sh
    	finder-app/written.txt

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,9 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char* hardcoded_username = my_username();
+    char* username_from_config = malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded_username, username_from_config, "Usernames do not match!");
+
+    free(username_from_config);
 }
